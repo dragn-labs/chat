70a15b0e4e1ad03ae3f896680f187c39db9d7896
Berkeley Andrus <berkeley.andrus@gmail.com>
2020-01-07 13:24:09 -0700

    Added smooth color changes to reflect Eve's mood

--- eve-interface/src/app/app.component.ts
+++ eve-interface/src/app/app.component.ts
@@ -14,8 +14,18 @@ export class AppComponent {
   eveTypingMessages = ['Eve is typing', 'Eve is typing.', 'Eve is typing..', 'Eve is typing...', 'Eve is typing....'];
   typingMessageIndex = 0;
 
+  moodsToColors = {'Sad': {r: 0xD9, b: 0xE5, g: 0xD6, mood: 'Sad'},
+                   'Upset': {r: 0xD3, b: 0x65, g: 0x77, mood: 'Upset'},
+                   'Happy': {r: 0x01, b: 0xBA, g: 0xEF, mood: 'Happy'},
+                   'Excited': {r: 0xFF, b: 0xFF, g: 0x72, mood: 'Excited'},
+                   'Neutral': {r: 0x7D, b: 0xDF, g: 0x64, mood: 'Neutral'}}
+
+  currentColor: Color = {r: 0x7D, b: 0xDF, g: 0x64, mood: ''};
+  targetColor: Color = this.moodsToColors['Neutral'];
+
   constructor() {
     this.updateMessageIndex();
+    this.updateMoodColor();
   }
 
   updateMessageIndex() {
@@ -26,23 +36,60 @@ export class AppComponent {
     }, 600);
   }
 
+  createColorString(color: Color): string {
+    var ret = '#';
+    ['r', 'b', 'g'].forEach(colorKey => {
+      var colorString = color[colorKey].toString(16);
+      if (colorString.length == 1) {
+        colorString = '0' + colorString;
+      }
+      ret = ret + colorString;
+    });
+    return ret;
+  }
+
+  updateMoodColor() {
+    setTimeout(() => {
+      var isEqual = true;
+      ['r', 'b', 'g'].forEach(colorKey => {
+        if (this.currentColor[colorKey] < this.targetColor[colorKey]) {
+          isEqual = false;
+          this.currentColor[colorKey]++;
+        } else if (this.currentColor[colorKey] > this.targetColor[colorKey]) {
+          isEqual = false;
+          this.currentColor[colorKey]--;
+        }
+      });
+      document.body.style.backgroundColor = this.createColorString(this.currentColor);
+      if (!isEqual) {
+        this.updateMoodColor();
+      }
+    }, 20);
+  }
+
   postUserMessage(message: string) {
     this.conversationHistory.push({text: message, from: 'User'});
     this.updateVisibleMessages();
-    setTimeout(() => this.postEveMessage(), 500);
+    if (Object.keys(this.moodsToColors).includes(message)) {
+      this.targetColor = this.moodsToColors[message];
+      this.updateMoodColor();
+      this.postEveMessage('I am ' + message);
+    } else {
+      setTimeout(() => this.postEveMessage(), 500);
+    }
   }
 
-  postEveMessage() {
+  postEveMessage(givenMessage?: string) {
     this.eveIsThinking = true;
     this.typingMessageIndex = 0;
     setTimeout(() => {
       // FIXME this is where I will send conversation to server to get the next response
       const cannedResponses = ['[Witty reply]', '[Pithy comment]', '[Helpful insight]', '[Clarifying question]', 'Yes, I agree', 'I am not so sure', 'Thanks, I needed that', 'You\'re a good friend.'];
       const randResponse = cannedResponses[Math.floor(Math.random() * cannedResponses.length)];
-      this.conversationHistory.push({text: randResponse, from: 'Eve'});
+      this.conversationHistory.push({text: givenMessage || randResponse, from: 'Eve'});
       this.eveIsThinking = false;
       this.updateVisibleMessages();
-    }, 2000 + Math.random() * 2000);
+    }, givenMessage ? 1000 : (2000 + Math.random() * 2000));
   }
 
   updateVisibleMessages() {
@@ -57,4 +104,11 @@ export class AppComponent {
 interface Message {
   text: string;
   from: 'User'|'Eve';
+}
+
+interface Color {
+  r: number;
+  b: number;
+  g: number;
+  mood: string;
 }
